4f28f51935ab69628d2feb4639c6ff6208faf521
explosic4 <explosic4@gmail.com>
2016-12-17 22:15:15 +0800

    Added utilities.py for tag, categorize, search by image

---
+++ web/apps/base/api_keys.py
@@ -0,0 +1,6 @@
+""" this file contains api key information """
+
+api_key = "acc_c532c8d94fa0eb6"
+api_secret = "524147fee9cfc8f1bb8318c17ddf9d8f"
+authorization = "Basic YWNjX2M1MzJjOGQ5NGZhMGViNjo1MjQxNDdmZWU5Y2ZjOGYxYmI4MzE4YzE3ZGRmOWQ4Zg=="
+categorizer_id = 'personal_photos'
